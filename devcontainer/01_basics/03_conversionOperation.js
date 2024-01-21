a9b0a750b4f498e6ea14981eb11ec100a36fb4a2
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-01-21 19:54:35 +0000

    converstion and Practice

--- devcontainer/01_basics/03_conversionOperation.js
+++ devcontainer/01_basics/03_conversionOperation.js
@@ -1,4 +1,59 @@
+// // // let score = 324345
+// // //     const condition = isNaN(score)
+// // //     if(!condition) {
+// // //         console.log('hello', condition)
+// // //     }
+
+// // //     const converted = ''+ score
+// // // console.log(typeof(converted));
+
+// // //immidiate invoke function expresion
+// //     (function abc () {
+// //         console.log('helo')
+// //     }())
+
+// //     function shubham () {
+// //         console.log('shubham')
+// //     }
+
+// //     const arrayFunc = () => {
+// //             return 2*10/5
+// //     }
+
+// //     console.log(arrayFunc())
+
+
+// //     (function tiwarijii () {
+// //         console.log('sam'); 
+// //     }());
+
+
+// //     (function tiwarijii() {
+// //         console.log('sam');
+// //     }());
+    
+// //     (function tiwarijii() {
+// //         console.log('sam');
+// //     }());
+
+// //const numbers = [5, 10, 15, 20, 25];
+
+// // Step 1: Double each number using map
+// const doubledNumbers = numbers.map(num => num * 2);
+
+// // Step 2: Filter out numbers less than 10 using filter
+// const filteredNumbers = doubledNumbers.filter(num => num >= 10);
+
+// // Step 3: Calculate the sum of the remaining numbers using reduce
+// const sum = filteredNumbers.reduce((acc, num) => acc + num, 0);
+
+// console.log("Original Numbers:", numbers);
+// console.log("Doubled Numbers:", doubledNumbers);
+// console.log("Filtered Numbers (>= 10):", filteredNumbers);
+// console.log("Sum of Filtered Numbers:", sum);//
+
+
+
 let score = 33
 
 console.log(typeof score);
-console.log(typeof (score));
---
+++ devcontainer/01_basics/04_practice.js
@@ -0,0 +1,31 @@
+let score = "shubham"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+// "33" => 33
+// "33xyz" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = "shubham"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "shubham" => true
+
+let someNumber = 86487
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
