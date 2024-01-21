6c44bd73e6462bde37413e13618c5425d557113d
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-01-21 21:22:54 +0000

    added

--- devcontainer/01_basics/04_practice.js
+++ devcontainer/01_basics/04_practice.js
@@ -1,31 +1,71 @@
-let score = "shubham"
+// let score = "shubham"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
-
-
-// "33" => 33
-// "33xyz" => NaN
-// true => 1; false => 0
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
-let isLoggedIn = "shubham"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// // "33" => 33
+// // "33xyz" => NaN
+// // true => 1; false => 0
 
-// 1 => true; 0 => false
-//"" => false
-// "shubham" => true
 
-let someNumber = 86487
+// let isLoggedIn = "shubham"
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// // 1 => true; 0 => false
+// //"" => false
+// // "shubham" => true
+
+// let someNumber = 86487
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//*************************************************Operations******************************/
+
+// let gtaVstorage = 106
+// let neggtaVstorage = -gtaVstorage
+// console.log(neggtaVstorage);
+
+
+let value = 5
+let negValue = -value
+console.log(negValue);
+
+// let str1 = "shubham"
+// let str2 = " tiwari"
+// let str3 = str1 + str2
+// console.log(str3);
+
+let str1 = "golu"
+let str2 = " haluwai"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1+ "2");
+console.log("1"+ 2 + 2);
+console.log(1+ 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
 
