c2b4d9d5a727ca617c678d4d37526374cdcd3a65
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-01-24 17:10:32 +0000

    memory

--- devcontainer/01_basics/datatypes_summary.js
+++ devcontainer/01_basics/datatypes_summary.js
@@ -1,36 +1,57 @@
-// //primitive
+// // //primitive
 
-// // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+// // // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
-// const score = 100
-// const scoreValue = 100.3
+// // const score = 100
+// // const scoreValue = 100.3
 
-// const isLoggedIn = false
-// const outsideTemp = null
-// let userEmail;
+// // const isLoggedIn = false
+// // const outsideTemp = null
+// // let userEmail;
 
-// const id = Symbol("123")
-// const anotherId = Symbol("123")
+// // const id = Symbol("123")
+// // const anotherId = Symbol("123")
 
-// console.log(id === anotherId);
+// // console.log(id === anotherId);
 
-// // const bigNumber = 1234567890987654321n
-
-
-
-// // Refernece(not primitive)
-// // Array, Objects, Functions
-
-// const heros = ["hulk","thor","iron man"];
-// let myObj = {
-//     name : "shubham",
-//     age : 19,
-// }
+// // // const bigNumber = 1234567890987654321n
 
 
-const myFunction = Function()
-    console.log("helo world");
+
+// // // Refernece(not primitive)
+// // // Array, Objects, Functions
+
+// // const heros = ["hulk","thor","iron man"];
+// // let myObj = {
+// //     name : "shubham",
+// //     age : 19,
+// // }
+
+
+// const myFunction = Function()
+//     console.log("helo world");
     
 
 
-console.log(typeof myFunction);
+// console.log(typeof myFunction);
+
+//types of memory in js
+//Stack(primitive), heap(non- primitive)
+
+let myYoutubename = "smtgamerz";
+let anotherName = myYoutubename
+anotherName = "Shubham Tiwari";
+
+console.log(anotherName);
+console.log(myYoutubename);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "shubham@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
