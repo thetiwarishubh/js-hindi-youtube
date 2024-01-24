8b2e67344822632be2fc6bd39f3ad1f9803f2d37
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-01-24 16:38:59 +0000

    datatypes summary

---
+++ devcontainer/01_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// //primitive
+
+// // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = Symbol("123")
+// const anotherId = Symbol("123")
+
+// console.log(id === anotherId);
+
+// // const bigNumber = 1234567890987654321n
+
+
+
+// // Refernece(not primitive)
+// // Array, Objects, Functions
+
+// const heros = ["hulk","thor","iron man"];
+// let myObj = {
+//     name : "shubham",
+//     age : 19,
+// }
+
+
+const myFunction = Function()
+    console.log("helo world");
+    
+
+
+console.log(typeof myFunction);
