9ea90adf740b6c2e20ffb0d71d929a95d5e58dff
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-02-03 06:54:26 +0000

    Global Scope & Block Scope

---
+++ devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+// var c = 4500
+let a = 5400
+
+if(true){
+    let a = 10
+    const b = 20
+    console.log("INNER:", a);
+    
+}
+
+console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+
