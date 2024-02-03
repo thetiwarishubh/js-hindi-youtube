8b7b9847f1d0b778dcf0cc4c9ab76fbb979b8741
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-02-03 18:52:26 +0000

    Scopes

--- devcontainer/03_basics/02_scopes.js
+++ devcontainer/03_basics/02_scopes.js
@@ -4,13 +4,49 @@ let a = 5400
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER:", a);
+    // console.log("INNER:", a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
 
+function one(){
+    const username = "shubham"
 
+    function Two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    Two()
+
+}
+
+// one()
+
+if(true){
+    const username = "shubham"
+    if(username === "shubham"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+
+}
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
 
