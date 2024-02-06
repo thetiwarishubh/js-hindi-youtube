4ad0e65a3e343aaf865073f1f874916593d010be
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-02-06 11:39:16 +0000

    one

--- devcontainer/04_control_flow/one.js
+++ devcontainer/04_control_flow/one.js
@@ -1,7 +1,45 @@
-//if
+// //if
+
+// const isUserLoggedIn = true
+// const temperature = 41
+
+// if(temperature === 41){
+//     console.log("less than 50");
+// }
+// else {
+//     console.log("temperature is greater than 50");
+// }
+
+// const score = 200
+// if(score > 100) {
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+
+// }
+//  console.log(`User power: ${power}`);
+
+// const  balance = 1000
+// if(balance > 500) console.log("test"), console.log("test2");
+
+// if(balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750){
+//     console.log("less than 750");
+// } else if (balance < 900) {
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+// }
 
 const isUserLoggedIn = true
-
-if(2 != 3){
-    console.log("executed");
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+ 
+if (isUserLoggedIn && debitCard) {
+    console.log("Allow to buy course");
+    
+}
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
 }
