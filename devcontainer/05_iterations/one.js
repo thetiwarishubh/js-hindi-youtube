ee5fa831a40dab61bdd9e384ae3e5dbc5f1dc943
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-02-07 15:11:27 +0000

    loops

---
+++ devcontainer/05_iterations/one.js
@@ -0,0 +1,26 @@
+// for 
+
+for (let shubham = 0; shubham <= 10; shubham++) {
+    const element = shubham;
+    if (element == 5) {
+        // console.log("5 is best number")
+    }
+    // console.log(element);
+    
+}
+
+for (let shubham = 1; shubham <= 20; shubham++) {
+    // console.log(`Outer loop value: ${shubham}`)
+    for (let tiwari = 1; tiwari <= 20; tiwari++) {
+    //    console.log(`Inner loop value: ${tiwari} and inner loop ${shubham}`)
+    // console.log(shubham + '*' + tiwari + ' = ' + shubham*tiwari)
+        
+    }
+}
+let myArray = ["flash", "batman", "superman"]
+console.log(myArray.length);
+
+for (let index = 0; index <= myArray.length; index++) {
+    const element = myArray[index];
+    console.log(element);
+}
