f5fed3096ab8e9d9f144bd07816c23b9a8102a78
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-02-07 17:16:22 +0000

    loops

--- devcontainer/05_iterations/one.js
+++ devcontainer/05_iterations/one.js
@@ -18,9 +18,30 @@ for (let shubham = 1; shubham <= 20; shubham++) {
     }
 }
 let myArray = ["flash", "batman", "superman"]
-console.log(myArray.length);
+// console.log(myArray.length);
 
 for (let index = 0; index <= myArray.length; index++) {
     const element = myArray[index];
-    console.log(element);
+    // console.log(element);
+}
+
+
+// break and continue 
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5) {
+//         console.log(`Detected 5`);
+//         break
+//     }
+//     console.log(`value of index is ${index}`);
+    
+// }
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log(`Detected 5`);
+        continue
+    }
+    console.log(`value of index is ${index}`);
+    
 }
