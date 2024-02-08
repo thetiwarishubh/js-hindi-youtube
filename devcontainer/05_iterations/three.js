6633983e803b452528e1384baa096130cb6bac93
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-02-08 11:33:13 +0000

    three

---
+++ devcontainer/05_iterations/three.js
@@ -0,0 +1,14 @@
+// for of loops
+
+const arr = [1, 2, 3, 4, 5]
+
+
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "Hello World!"
+
+for (const greet of greetings) {
+    console.log(`Each character is ${greet}`);
+}
