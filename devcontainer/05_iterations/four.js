4fceea3099b2641f93ec9bfb78e32c483609b86b
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-02-08 17:47:19 +0000

    loops

---
+++ devcontainer/05_iterations/four.js
@@ -0,0 +1,27 @@
+const myObject = {
+    js: 'JavaScript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift by apple'
+}
+
+for (const key in myObject) {
+//    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "java", "rb", "py", "cpp"]
+
+for (const key in programming) {
+    // console.log(programming[key]);
+  
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States Of America")
+// map.set('FR', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
--- devcontainer/05_iterations/three.js
+++ devcontainer/05_iterations/three.js
@@ -1,6 +1,6 @@
 // for of loops
 
-const arr = [1, 2, 3, 4, 5]
+/*const arr = [1, 2, 3, 4, 5]
 
 
 for (const num of arr) {
@@ -11,4 +11,27 @@ const greetings = "Hello World!"
 
 for (const greet of greetings) {
     console.log(`Each character is ${greet}`);
+}*/
+
+// Maps
+
+const map = new Map()
+map.set('IN', "India")
+map.set('USA', "United States Of America")
+map.set('FR', "France")
+map.set('IN', "India")
+
+// console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, ':-', value);
 }
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'GTA V'
+}
+
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// }
