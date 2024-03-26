57a0c29da606fcad4796455b8dceafb6b7443146
thetiwarishubh <157212411+thetiwarishubh@users.noreply.github.com>
2024-03-26 17:21:26 +0000

    datatypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as new JS Version
+
+// alert( 3 + 3) we are using nodejs, not browser
+
+// console.log(3 + 3); console.log("shubham"); code readability should be high
+
+let name = "shubham"
+let age = 19
+let isLoggedIn = true
+
+//number => 2 to power 53
+//bigInt
+//string => "" or ''
+//boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof undefined);
+
+//important 
+// typeof null is object
